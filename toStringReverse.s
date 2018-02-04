83052a860ac6c56ecbf4625c6f98cbf1a6a9030a
KevinLourie <kzlourie>
2018-02-03 16:02:53 -0800

    Getting the hang of writing comments

--- toStringReverse.s
+++ toStringReverse.s
@@ -68,54 +68,55 @@ $L3:
 	nop
 	.set	reorder
 $L4:
-	lw	$5,16($fp)			
-	move	$3,$5				# Move contents of r5 into r3
-	lw	$2,36($fp)			
-	addu	$6,$3,$2			# Add r3 to r2, store in r6
-	lw	$4,32($fp)		
+	# s[i++] = n % 10 + '0';
+	lw	$5,16($fp)			# Load r5 with n
+	move	$3,$5				# Move contents n into r3
+	lw	$2,36($fp)			# Load r2 with s
+	addu	$6,$3,$2			# Compute n + s and store in r6
+	lw	$4,32($fp)			# Load r4 with n
 	li	$2,1717960704			# 0x66660000
 	ori	$2,$2,0x6667
-	mult	$4,$2				# Multiply r4 by r2
+	mult	$4,$2				# Compute n * s
 	mfhi	$2				
 	sra	$3,$2,2
 	sra	$2,$4,31
-	subu	$3,$3,$2
-	move	$2,$3				# Move contents of r3 into r2
+	subu	$3,$3,$2			# Compute n - s and store in r3
+	move	$2,$3				# Move n into r2
 	sll	$2,$2,2
-	addu	$2,$2,$3			# Add r2 to r3 and store in r2
+	addu	$2,$2,$3			# Compute n + s and store in r2
 	sll	$2,$2,1
-	subu	$2,$4,$2			# Subtract contents of r4 by contents of r2 and store in r2
-	addu	$2,$2,48			# Add 48 to r2 and store in r2
+	subu	$2,$4,$2			# Compute n - s and store in r2
+	addu	$2,$2,48			# Add 48 to s and store in r2
 	sb	$2,0($6)			
-	addu	$5,$5,1				# Add 1 to r5 and store in r5
-	sw	$5,16($fp)			# Save second parameter n
-	lw	$4,32($fp)			# Load first parameter s
+	addu	$5,$5,1				# Add 1 to n and store in r5
+	sw	$5,16($fp)			# Store r5 into n
+	lw	$4,32($fp)			# Load r4 with n
 	li	$2,1717960704			# 0x66660000
 	ori	$2,$2,0x6667
-	mult	$4,$2				# Multiply contents of r4 by r2
+	mult	$4,$2				# Compute n * s
 	mfhi	$2	
 	sra	$3,$2,2		
 	sra	$2,$4,31
-	subu	$2,$3,$2	`		# Subtract contents of r3 by r2 and store in r2
-	sw	$2,32($fp)			
+	subu	$2,$3,$2			# Compute (n - s) - s and store in r2
+	sw	$2,32($fp)			# Store n into r2
 	bgtz	$2,$L4
-	lw	$2,20($fp)
-	bgez	$2,$L8				# Branch is r2 is greater than 0
-	lw	$3,16($fp)
-	move	$4,$3				# Move r3 into r4
-	lw	$2,36($fp)
-	addu	$4,$4,$2			# Add r4 and r2 and store in r2
+	lw	$2,20($fp)			# Load r2 with sign
+	bgez	$2,$L8				# Branch if sign is greater than 0
+	lw	$3,16($fp)			# Load r3 with n
+	move	$4,$3				# Move (n - s) into r4
+	lw	$2,36($fp)			# Load r2 with s
+	addu	$4,$4,$2			# Compute (n - s) + s and store in r2
 	li	$2,45			# 0x2d
-	sb	$2,0($4)
+	sb	$2,0($4)			# Store r2 into (n - s)
 	addu	$3,$3,1				# Add 1 to r3 and store in r3
-	sw	$3,16($fp)
+	sw	$3,16($fp)			# Store r3 into n
 $L8:
 	lw	$2,36($fp)		# Load r2 with  n % 10
-	lw	$3,16($fp)		# Load r3 with '\0'
+	lw	$3,16($fp)		# Load r3 with n
 	addu	$2,$2,$3		# Add '0' to n % 10
 	sb	$0,0($2)
 	lw	$4,36($fp)		# Load lst parameter n into r4
-	lw	$5,16($fp)		# Load 2nd parameter s into r5
+	lw	$5,16($fp)		# Load r5 with n
 	jal	reverse_string		# Call reverse string
 	move	$sp,$fp 		# Restore stack
 	lw	$31,28($sp)
@@ -134,52 +135,59 @@ reverse_string:
 	sw	$fp,16($sp)		
 	move	$fp,$sp
 	# char* reverse_string(char *str, int len)
-	sw	$4,24($fp)		# Store 1st parameter str into r4
-	# int i;
-	lw	$2,28($fp)		# Load r2 with i
+	sw	$4,24($fp)		# Store 1st parameter in str
+	sw	$5,28($fp)		# Store 2nd parameter in len
 	# int k = len - 1;
-	addu	$2,$2,-1		# k = len - 1
-	sw	$2,4($fp)		
-	sw	$0,0($fp)		
+	lw	$2,28($fp)		# Load r2 with len
+	addu	$2,$2,-1		# Compute len - 1
+	sw	$2,4($fp)		# Store result in k
+	# i = 0 
+	sw	$0,0($fp)		# Store 0 in i
 $L10:
-	lw	$3,28($fp)
-	sra	$2,$3,31
-	srl	$2,$2,31
+	# i < len/2
+	lw	$3,28($fp)		# Load r3 with len
+	sra	$2,$3,31		# Compute len / 2
+	srl	$2,$2,31	
 	addu	$2,$3,$2
 	sra	$3,$2,1
-	lw	$2,0($fp)
-	slt	$2,$2,$3
-	bne	$2,$0,$L13
+	lw	$2,0($fp)		# Load r2 with i
+	slt	$2,$2,$3		# Compute i < len / 2 and store in r2
+	bne	$2,$0,$L13		# Branch if result is not equal to zero
 	j	$L11
 $L13:
-	lw	$3,24($fp)
-	lw	$2,4($fp)
-	addu	$2,$3,$2			# Add r2 and r3 and store in r2
-	lbu	$2,0($2)
-	sb	$2,8($fp)	
-	lw	$3,24($fp)
-	lw	$2,4($fp)
-	addu	$4,$3,$2			# Add r3 and r2 and store in r4
-	lw	$3,24($fp)
-	lw	$2,0($fp)
-	addu	$2,$3,$2			# Add r3 and r2 and store in r2
-	lbu	$2,0($2)	
-	sb	$2,0($4)
-	lw	$3,24($fp)
-	lw	$2,0($fp)
-	addu	$3,$3,$2
-	lbu	$2,8($fp)
-	sb	$2,0($3)
-	lw	$2,4($fp)
-	addu	$2,$2,-1			# Subtract 1 from r2
-	sw	$2,4($fp)
-	lw	$2,0($fp)			
-	addu	$2,$2,1				# Add 1 to r2
-	sw	$2,0($fp)
-	j	$L10
+	# char temp = str[k]
+	lw	$3,24($fp)		# Load r3 with str
+	lw	$2,4($fp)		# Load r2 with k
+	addu	$2,$3,$2		# Compute str + k and store in r2
+	lbu	$2,0($2)		# Load r2 with str[k]
+	sb	$2,8($fp)		# Store r2 in temp
+	# str[k] = str[i]
+	lw	$3,24($fp)		# Load r3 with str
+	lw	$2,4($fp)		# Load r2 with k
+	addu	$4,$3,$2		# Compute str + k and store in r4
+	lw	$3,24($fp)		# Load r3 with str
+	lw	$2,0($fp)		# Load r2 with i
+	addu	$2,$3,$2		# Compute str + i and store in r2
+	lbu	$2,0($2)		# Load r2 with str[i]
+	sb	$2,0($4)		# Store r2 in str[k]
+	# str[i] = temp;
+	lw	$3,24($fp)		# Load r3 with str
+	lw	$2,0($fp)		# Load r2 with i
+	addu	$3,$3,$2		# Compute str + i and store in r3
+	lbu	$2,8($fp)		# Load r2 with temp
+	sb	$2,0($3)		# Store r2 in str[i]
+	# k--;
+	lw	$2,4($fp)		# Load r2 with k
+	addu	$2,$2,-1		# Compute k - 1
+	sw	$2,4($fp)		# Store result in k
+	# i++
+	lw	$2,0($fp)		# Load r2 with i
+	addu	$2,$2,1			# Increment r2 
+	sw	$2,0($fp)		# Store r2 in i
+	j	$L10			# Loop back
 $L11:
-	move	$sp,$fp
-	lw	$fp,16($sp)
-	addu	$sp,$sp,24
-	jr	$31
-	.end	reverse_string
+	move	$sp,$fp			# Restore stack
+	lw	$fp,16($sp)		# Restore frame pointer
+	addu	$sp,$sp,24		# Remove 6 words on stack	
+	jr	$31			# Halt
+	.end	reverse_string		# End method
